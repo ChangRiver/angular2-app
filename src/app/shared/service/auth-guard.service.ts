d8835874cc1c91ba8e3957777194eebe9f44c24c
changriver <changriver@suboat.com>
2016-11-14 14:30:58 +0800

    fix bugger

--- src/app/shared/service/auth-guard.service.ts
+++ src/app/shared/service/auth-guard.service.ts
@@ -1,18 +1,19 @@
 import { Injectable } from '@angular/core';
 import { UserService } from './user.service';
-import { CanActivate, RouterStateSnapshot, ActivatedRouteSnapshot } from '@angular/router';
+import { CanActivate, RouterStateSnapshot, ActivatedRouteSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs/Rx';
 
 @Injectable()
-export class AuthGuard implements CanActivate{
+export class AuthGuard implements CanActivate {
 
   constructor(
-    private userService: UserService
+    private userService: UserService,
+    private router: Router
   ) {}
 
   canActivate(
-    private route: RouterStateSnapshot,
-    private state: ActivatedRouteSnapshot
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
   ): Observable<boolean> {
 
     return this.userService.isAuthenticated.take(1);
